3d3d64f33c86e677850e9fd6ce95af5b06fe48c2
Paolo Lago <paolo@recosapp.com>
2021-01-24 18:32:59 -0600

    cleaning up

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-import FacebookScreen from './screens/FacebookScreen'
-import TwitterScreen from './screens/TwitterScreen'
 
 const App = () => {
    return (
@@ -14,8 +12,6 @@ const App = () => {
          <main className='py-3'>
             <Container>
                <Route path='/' component={HomeScreen} exact />
-               <Route path='/twitter' component={TwitterScreen} exact />
-               <Route path='/facebook' component={FacebookScreen} exact />
             </Container>
          </main>
          <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,12 +19,43 @@ const Header = () => {
                <Navbar.Toggle aria-controls='basic-navbar-nav' />
                <Navbar.Collapse id='basic-navbar-nav'>
                   <Nav className='ml-auto'>
-                     <LinkContainer to='/twitter'>
-                        <Nav.Link>Twitter</Nav.Link>
+                     <LinkContainer to='/cart'>
+                        <Nav.Link>
+                           <i className='fas fa-shopping-cart'></i> Cart
+                        </Nav.Link>
                      </LinkContainer>
-                     <LinkContainer to='/facebook'>
-                        <Nav.Link>Facebook</Nav.Link>
-                     </LinkContainer>
+                     {userInfo?.name ? (
+                        <NavDropdown title='Username' id='username'>
+                           <LinkContainer to='/profile'>
+                              <NavDropdown.Item>Profile</NavDropdown.Item>
+                           </LinkContainer>
+
+                           <NavDropdown.Item
+                              onClick={(_) => console.log('Logout')}
+                           >
+                              Logout
+                           </NavDropdown.Item>
+                        </NavDropdown>
+                     ) : (
+                        <LinkContainer to='/login'>
+                           <Nav.Link>
+                              <i className='fas fa-user'></i> Sign In
+                           </Nav.Link>
+                        </LinkContainer>
+                     )}
+                     {userInfo && userInfo.isAdmin && (
+                        <NavDropdown title='Admin' id='admin-menu'>
+                           <LinkContainer to='/admin/userlist'>
+                              <NavDropdown.Item>Users</NavDropdown.Item>
+                           </LinkContainer>
+                           <LinkContainer to='/admin/productlist'>
+                              <NavDropdown.Item>Products</NavDropdown.Item>
+                           </LinkContainer>
+                           <LinkContainer to='/admin/orderlist'>
+                              <NavDropdown.Item>Orders</NavDropdown.Item>
+                           </LinkContainer>
+                        </NavDropdown>
+                     )}
                   </Nav>
                </Navbar.Collapse>
             </Container>
